c2afa324c12c2ab1e758f7e6d1d7e17b4434a06f
Smart developer <kolesnikova.ek21@yandex.ru>
2023-02-06 19:29:15 +0300

    commit

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import sys
 
-from Samples.geocoder import get_coordinates, get_ll_span
-from Samples.mapapi_PG import show_map
+from Samples.geocoder import get_coordinates, get_nearest_object
 
 
 def main():
-    toponym_to_find = " ".join(sys.argv[1:])
-
-    if toponym_to_find:
-        # Показываем карту с фиксированным масштабом.
-        lat, lon = get_coordinates(toponym_to_find)
-        ll_spn = f"ll={lat},{lon}&spn=0.005,0.005"
-        show_map(ll_spn, "map")
-
-        # Показываем карту с масштабом, подобранным по заданному объекту.
-        ll, spn = get_ll_span(toponym_to_find)
-        ll_spn = f"ll={ll}&spn={spn}"
-        show_map(ll_spn, "map")
-
-        # Добавляем исходную точку на карту.
-        point_param = f"pt={ll}"
-        show_map(ll_spn, "map", add_params=point_param)
-    else:
+    # Забираем адресную точку из параметров запуска.
+    address = ''
+    try:
+        address = " ".join(sys.argv[1:])
+    except:
         print('No data')
+        exit(1)
+
+    if not address:
+        print('No data')
+        exit(1)
+
+    # Получаем координаты точки
+    address_point = get_coordinates(address)
+
+    # Получаем район.
+    district_name = get_nearest_object(address_point, "district")
+    print(district_name)
 
 
 if __name__ == "__main__":
