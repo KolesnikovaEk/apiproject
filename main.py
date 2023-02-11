1316af8880e4d8f5b0b761d2c6ac8ff1ee106325
NLarinov <tkachevkolya2006@gmail.com>
2023-02-11 10:17:24 +0300

    Debug + Address

--- main.py
+++ main.py
@@ -7,73 +7,91 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 import requests
 from PIL import Image
 
-SCREEN_SIZE = [600, 450]
+SCREEN_SIZE = [900, 450]
+
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.pixmap = None
+        self.map_file = None
+        self.toponym_longitude = None
+        self.toponym_lattitude = None
+
         uic.loadUi('file.ui', self)
         self.adr = 'Москва, ул.Тверская'
         self.delta = "0.005"
         self.up = 0
         self.right = 0
-        self.getImage()
+        self.get_image()
         self.initUI()
-        #self.pushButton.clicked.connect(self.get_adress)
+
+        self.findbutton.clicked.connect(self.get_adress)
+        self.deletebutton.clicked.connect(self.delete)
         self.pushButton_up.clicked.connect(self.do_plus)
         self.pushButton_down.clicked.connect(self.do_minus)
 
+    def delete(self):
+        self.findline.clear()
+        self.adr = 'Москва, ул.Тверская'
+        self.get_image()
+        self.initUI()
+        # remove mark
 
     def do_up(self):
         self.up = self.up + float(self.delta)
-        self.getImage()
+        self.get_image()
         self.initUI()
 
     def do_down(self):
         self.up = self.up - float(self.delta)
-        self.getImage()
+        self.get_image()
         self.initUI()
 
     def do_right(self):
         self.right = self.up + float(self.delta)
-        self.getImage()
+        self.get_image()
         self.initUI()
 
     def do_left(self):
         self.right = self.right - float(self.delta)
-        self.getImage()
+        self.get_image()
         self.initUI()
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Up:
+        if event.key() == Qt.Key_W:
             self.do_up()
-        elif event.key() == Qt.Key_Down:
+        elif event.key() == Qt.Key_S:
             self.do_down()
-        elif event.key() == Qt.Key_Right:
+        elif event.key() == Qt.Key_D:
             self.do_right()
-        elif event.key() == Qt.Key_Left:
+        elif event.key() == Qt.Key_A:
             self.do_left()
+        elif event.key() == Qt.Key_Plus:
+            self.do_plus()
+        elif event.key() == Qt.Key_Minus:
+            self.do_minus()
 
     def get_adress(self):
-        self.adr = self.lineEdit.text()
-        self.getImage()
-        self.initUI()
-
-    def do_plus(self):
-        self.delta = str(float(self.delta) + 0.005)
-        if float(self.delta) > 1:
-            self.delta = '1'
-        self.getImage()
+        self.adr = self.findline.text()
+        self.get_image()
         self.initUI()
 
     def do_minus(self):
+        self.delta = str(float(self.delta) + 0.005)
+        if float(self.delta) > 1:
+            self.delta = '1'
+        self.get_image()
+        self.initUI()
+
+    def do_plus(self):
         self.delta = str(float(self.delta) - 0.005)
         if float(self.delta) < 0.00001:
             self.delta = '0.00001'
-        self.getImage()
+        self.get_image()
         self.initUI()
 
-    def getImage(self):
+    def get_image(self):
         toponym_to_find = self.adr
         geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
         geocoder_params = {
@@ -89,7 +107,8 @@ class Example(QWidget):
         toponym_coodrinates = toponym["Point"]["pos"]
         self.toponym_longitude, self.toponym_lattitude = toponym_coodrinates.split(" ")
         map_params = {
-            "ll": ",".join([str(float(self.toponym_longitude) + self.up), str(float(self.toponym_lattitude) + self.right)]),
+            "ll": ",".join([str(float(self.toponym_longitude) + self.up),
+                            str(float(self.toponym_lattitude) + self.right)]),
             "spn": ",".join([self.delta, self.delta]),
             "l": "map"
         }
@@ -111,4 +130,3 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
-
